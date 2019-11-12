c3c229edfffc1c39d7e5275f00632584e5c4163e
Leandro Carolino Dos Santos <leandro@watermelon.co>
2019-11-12 17:11:52 +0100

    data shared added

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const express = require('express');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(adminRoutes);
+app.use('/admin',adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
